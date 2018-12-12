e03ba48804e787258ce4c874ad63d4037ddbd862
Joel W Kall <joel@loop54.com>
2018-12-12 20:44:45 +0100

    Created Unity project and started experimenting

---
+++ Assets/Bullet.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+
+    private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
+    private float lifeTime;
+
+    // Use this for initialization
+    void Start () {
+
+        var renderer = GetComponent<SpriteRenderer>();
+
+        renderer.sprite = Resources.Load("Sprites/Circle", typeof(Sprite)) as Sprite;
+        renderer.color = Color.cyan;
+        
+        gameObject.transform.localScale = new Vector3(0.4f, 0.4f);
+
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.mass = 0.01f;
+
+        var mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var diff = new Vector2(mouse.x, mouse.y) - rb2d.position;
+
+        rb2d.AddForce(diff.normalized * 5f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        lifeTime += Time.deltaTime;
+
+        if (lifeTime > 1)
+            Destroy(gameObject);
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speedMultiplier;             //Floating point variable to store the player's movement speed.
+    public float maxSpeed;
+    public float jumpStrength;
+    public float rateOfFire;
+
+    private Rigidbody2D rb2d;   
+    private BoxCollider2D collider;
+
+    private float timeSinceLastFire;
+
+	// Use this for initialization
+	void Start () {
+		//Get and store a reference to the Rigidbody2D component so that we can access it.
+        rb2d = GetComponent<Rigidbody2D>();
+        collider = GetComponent<BoxCollider2D>();
+	}
+	
+	//FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+    void FixedUpdate()
+    {
+        //Store the current horizontal input in the float moveHorizontal.
+        var moveHorizontal = Input.GetAxis("Horizontal");
+
+        //Use the two store floats to create a new Vector2 variable movement.
+        var movement = new Vector2(moveHorizontal, 0);
+
+        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
+        if (Mathf.Abs(rb2d.velocity.x + movement.x) < maxSpeed)
+            rb2d.AddForce (movement * speedMultiplier);
+
+        if (Input.GetButton("Jump"))
+        {
+            var jumpMovement = new Vector2(0, jumpStrength);
+            rb2d.AddForce(jumpMovement);
+        }
+
+        timeSinceLastFire += Time.deltaTime;
+
+        if(Input.GetMouseButton(0) && timeSinceLastFire > rateOfFire)
+        {
+            var go = new GameObject("Bullet");
+            go.AddComponent<SpriteRenderer>();
+            go.AddComponent<Rigidbody2D>();
+            go.AddComponent<Bullet>();
+            
+            go.transform.position = rb2d.position;
+
+            var bulletCollider = go.AddComponent<CircleCollider2D>();
+            Physics2D.IgnoreCollision(collider, bulletCollider);
+
+            timeSinceLastFire = 0;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
